3b863d6d885cbaf63654951adc09a3260b8b4400
unknown <jwende20@g.holycross.edu>
2021-01-31 20:16:28 -0500

    Plotly Mini Project

--- app.py
+++ app.py
@@ -12,6 +12,9 @@ def index():
 @app.route("/samples")
 def samples():
     # open the json file, located at static/data/samples.json
+    # use json.load() to read in the file as json
+    # return json through Flask endpoint
+    
     file - open("static/data/samples.json", encoding='utf-8')
     data = json.load(file)
     return data
